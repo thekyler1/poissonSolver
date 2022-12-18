4653f45318e254fa4665f8c1a92b93420bc0e380
thekyler1 <necati.isik95@gmail.com>
2022-12-18 21:29:47 +0300

    main -> removed (redundancy)

--- src/parametric_sim.m
+++ src/parametric_sim.m
@@ -1,35 +1,36 @@
 
-k = linspace(-0.1,0.3,100   );
+k = linspace(-0.2,0.1,26);
+len = length(k);
+out = zeros(1,len);
 
-for j = 1:100
+for j = 1:len   
 
-    
-    aa = simCat;
-    aa.setSimParameters(k(j), 90);
+    mySim = simCat;
+    mySim.setSimParameters(k(j), 90);
 
-    aa.newLayer;
-    aa.layerProp(3e-6,1e16,'n','mct',0.3);
-    aa.endLayer;
+    mySim.newLayer;
+    mySim.layerProp(3e-6,1e16,'n','mct',0.3);
+    mySim.endLayer;
 
-    % aa.newLayer;
-    % aa.layerProp(0.2e-6,1e18,'n','mct',0.35);
-    % aa.endLayer;
+    % mySim.newLayer;
+    % mySim.layerProp(0.2e-6,1e18,'n','mct',0.35);
+    % mySim.endLayer;
 
-    aa.newLayer;
-    aa.layerProp(1e-6,2e17,'p','mct',0.5);
-    aa.endLayer;
+    mySim.newLayer;
+    mySim.layerProp(1e-6,2e17,'p','mct',0.5);
+    mySim.endLayer;
 
-    aa.simHandle;
+    mySim.simHandle;
 
-    aa.deriveScaling;
-    aa.initConditions;
+    mySim.deriveScaling;
+    mySim.initConditions;
 
-    aa.startSim(1000);
+    mySim.startSim(1000);
 
-    aa.sim1.chargesDerivative;
-    aa.sim1.findTotCurrent;
+    mySim.sim1.chargesDerivative;
+    mySim.sim1.findTotCurrent;
 
-    x(j) = aa.sim1.totCurrent
+    out(1,j) = mySim.sim1.totCurrent;
 
 end
 
